a4a88ba6595a2a3db6feeb0dc69095d457c342ac
daniel1007 <daniel7oficial@gmail.com>
2020-04-16 17:39:22 -0300

    Atualizacao no app

--- src/conversor/index.js
+++ src/conversor/index.js
@@ -3,11 +3,12 @@ import {
   StyleSheet,
   View,
   Text,
-  Picker,
   TextInput,
   TouchableOpacity,
   Keyboard
 } from 'react-native';
+
+import {Picker} from '@react-native-community/picker';
 
 import api from '../services/api';
 
@@ -35,7 +36,8 @@ class Conversor extends Component{
     }
 
     async converter(){
-        let de_para = this.state.moedaA + '_' + this.state.moedaB;
+
+        let de_para = this.state.moedas[this.state.moedaA].valor + '_' + this.state.moedas[this.state.moedaB].valor;
         const response = await api.get(`convert?q=${de_para}&compact=ultra&apiKey=42be2a924ef783a2fde4`);
         let cotacao = response.data[de_para];
 
@@ -50,59 +52,64 @@ class Conversor extends Component{
 
     render(){
         //const {moedaA, moedaB} = this.props
-       
-        const moedasItems = this.state.moedas.map( (v, k) => {
-           return <Picker.Item key={k} name={v} label={v.valor} />
-        })
+
+        let moedasItems = this.state.moedas.map( (v, index) => {
+            return <Picker.Item key={index} value={index} label={v.valor} />
+         }) 
 
         return(
             
                 <View style={styles.container}>
 
-                    <View style={styles.pickers}>
-                    
-                        <Picker
-                            selectedValue={this.state.moedaA}
-                            style={{ height: 50, width: 100 }}
-                            onValueChange={(itemValue, itemIndex) => this.setState( { moedaA: itemValue } )}
-                        >
-                            {moedasItems}
-                        </Picker>                   
+                    <Text style={styles.titulo}>Conversor de Moedas</Text>
 
-                        <Text style={styles.titulo}>PARA</Text>
+                    
+
+                    <View style={styles.container}>
+                        <View style={styles.pickers}>
                         
-                        <Picker
-                            selectedValue={this.state.moedaB}
-                            style={{ height: 50, width: 100, marginLeft: 15 }}
-                            onValueChange={(itemValue, itemIndex) => this.setState( { moedaB: itemValue } )}
-                        >
-                            {moedasItems}
-                        </Picker>    
-                    
-                    </View>
+                            <Picker
+                                selectedValue={this.state.moedaA}
+                                style={{ height: 50, width: 100 }}
+                                onValueChange={(itemValue, itemIndex) => this.setState( { moedaA: itemValue } )}
+                            >
+                                {moedasItems}
+                            </Picker>                   
 
-                    <View >
-                        <Text style={{ fontSize: 20, marginTop: 20 }}>Converter 
-                            <Text style={{ fontWeight: 'bold' }}> {this.state.moedas[this.state.moedaA].name} </Text>
-                            para 
-                            <Text style={{ fontWeight: 'bold' }}> {this.state.moedas[this.state.moedaB].name} </Text>
+                            <Text style={styles.description}>PARA</Text>
+                            
+                            <Picker
+                                selectedValue={this.state.moedaB}
+                                style={{ height: 50, width: 100, marginLeft: 15 }}
+                                onValueChange={(itemValue, itemIndex) => this.setState( { moedaB: itemValue } )}
+                            >
+                                {moedasItems}
+                            </Picker>    
+                        
+                        </View>
+
+                        <View >
+                            <Text style={{ fontSize: 20, marginTop: 20 }}>Converter 
+                                <Text style={{ fontWeight: 'bold' }}> {this.state.moedas[this.state.moedaA].name} </Text>
+                                para 
+                                <Text style={{ fontWeight: 'bold' }}> {this.state.moedas[this.state.moedaB].name} </Text>
+                            </Text>
+                        </View>
+                
+                        <TextInput 
+                            placeholder="Valor a ser convertido"
+                            style={styles.areaInput}
+                            onChangeText={ (moedaB_valor) => this.setState({moedaB_valor}) }
+                            keyboardType="numeric"
+                        />
+                        <TouchableOpacity style={styles.botaoArea} onPress={this.converter}>
+                            <Text style={styles.botaoTexto}>Converter</Text>
+                        </TouchableOpacity>
+
+                        <Text style={styles.valorConvertido}>
+                            {(this.state.valorConvertido === 0) ? '' : this.state.valorConvertido}
                         </Text>
                     </View>
-            
-                    <TextInput 
-                        placeholder="Valor a ser convertido"
-                        style={styles.areaInput}
-                        onChangeText={ (moedaB_valor) => this.setState({moedaB_valor}) }
-                        keyboardType="numeric"
-                    />
-                    <TouchableOpacity style={styles.botaoArea} onPress={this.converter}>
-                        <Text style={styles.botaoTexto}>Converter</Text>
-                    </TouchableOpacity>
-
-                    <Text style={styles.valorConvertido}>
-                        {(this.state.valorConvertido === 0) ? '' : this.state.valorConvertido}
-                    </Text>
-            
             
             
                 </View>
@@ -120,7 +127,15 @@ const styles = StyleSheet.create({
     titulo:{
         fontSize: 30,
         fontWeight: 'bold',
-        color: '#000'
+        color: '#000',
+        justifyContent: "flex-start",
+        marginTop: 20
+    },
+    description:{
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#000',
+        paddingTop: 15
     },
     pickers:{
         justifyContent: 'space-around',
